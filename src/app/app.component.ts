4ca6aba3392a933606059d17fcd78e9edb7d3470
singhAvneet <asingh@gms.ca>
2023-10-24 12:47:49 -0600

    rollback responseoverride

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello secure 4 {{value}}</div>`,
+  template: `<div>Hello secure 5 {{value}}</div>`,
 })
 export class AppComponent {
   value = 'GMS';
