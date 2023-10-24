7121bd5f4aa9f8133eb89cd18a855bc9073c596a
singhAvneet <asingh@gms.ca>
2023-10-24 10:57:25 -0600

    more change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello secure {{value}}</div>`,
+  template: `<div>Hello secure 2 {{value}}</div>`,
 })
 export class AppComponent {
   value = 'GMS';
